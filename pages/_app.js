95a81f7b5e8c811bd69fe6e698a36a75c7acf402
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2022-07-17 15:33:25 -0700

    initial repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,74 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  lightTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+/* adding gnosis network */
+const GnosisChain = {
+  id: 100,
+  name: 'Gnosis Chain',
+  network: 'Gnosis',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'xDai',
+    symbol: 'xDai',
+  },
+  rpcUrls: {
+    default: 'https://rpc.ankr.com/gnosis',
+  },
+  blockExplorers: {
+    default: { name: 'Blockscout', url: 'https://blockscout.com/xdai/mainnet' },
+  },
+  iconUrls: ["https://images.prismic.io/koinly-marketing/16d1deb7-e71f-48a5-9ee7-83eb0f7038e4_Gnosis+Chain+Logo.png"],
+  testnet: false,
 }
 
-export default MyApp
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+   // alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        coolMode  //<<<<<<<< coolMode
+        chains={chains}
+        theme={lightTheme({
+          accentColor: "#623485", 
+          accentColorForeground: "white",
+          borderRadius: "large",
+          fontStack: "system",
+        })}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
